20ee8b7b29012c193f0f40a70acfaba17c85e2ac
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-06-13 20:42:37 +0100

    Delete Basic JavaScript Projects directory

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++
@@ -1,8 +0,0 @@
-//main.js
-
-//a function that uses the+= operator
-function myFunction() {
-    var sentence = "I am learning"; //creating the variable
-    sentence += " a lot from this course!"; //using the += operator to add to the variable
-    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementById()
-}
